1020d134b8ba5a346c4363cc8f6e4c975ff49c13
jaomaria <165792797+jaomaria@users.noreply.github.com>
2024-06-21 09:49:54 -0300

    Add files via upload

---
+++ formulario/script.js
@@ -0,0 +1,21 @@
+function validateForm(){
+    var fname = document.getElementById('fname').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var message = document.getElementById('message')value.trim();
+
+    if(fname === ' '){
+        alert('Preencha o campo Nome!');
+        return false;
+    }
+    if(email === ' '){
+        alert('Preencha o campo E-mail!');
+        return false;
+    }
+    if(message === ' '){
+        alert('Preencha o campo Mensagem!');
+        return false;
+    }
+
+    return true;
+
+}
